ea017298824e5ddb62c2ac8039db5e52dbc865dc
gu1ma <gabrielgs1408@gmail.com>
2018-04-21 20:54:10 -0300

    [Add] Requisicao para busca de CEP

--- src/components/CepChecker.vue
+++ src/components/CepChecker.vue
@@ -1,8 +1,14 @@
 <template>
     <section class="cep-checker">
         <label>Digite o seu cep</label>
-        <input type="text"/>
+        <input type="text" @blur="checkCep"/>
         <router-link class="home" to="/">Ver tarefas</router-link>
+        <div class="address" v-show="hasAddress()">
+            <p>Rua: {{this.address.logradouro}}</p>
+            <p>Bairro: {{this.address.bairro}}</p>
+            <p>Cidade: {{this.address.cidade}}</p> 
+            <p>Estado: {{address.estado}}</p>
+        </div>
     </section>
 </template>
 
@@ -10,7 +16,25 @@
 export default {
     data(){
         return {
+          address: {
+          }
         }
+    },
+    methods: {
+      checkCep($event){
+        let cep = $event.target.value;
+        this.$http.get('http://api.postmon.com.br/v1/cep/'+cep).then(
+          response => {
+            this.address = response.body
+          }, 
+          response => {
+            console.log(response)
+          }
+        )
+      },
+      hasAddress(){
+        return Object.keys(this.address).length > 0
+      }
     }
 }
 </script>
